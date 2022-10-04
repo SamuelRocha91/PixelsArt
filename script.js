d233620974d8b29c5fa23afe54759679d4cf1a86
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-10-04 16:37:25 -0300

    local storage - salvadno e recuperado

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ paleta3.style.backgroundColor = 'blue';
 let paleta4 = document.getElementsByClassName('color')[3];
 paleta4.style.backgroundColor = 'green';
 
-// Implementando o quarto requisito
+// Implementando o quarto requisito e o quinto 1a parte
 
 const botao = document.createElement('button');
 botao.id = 'button-random-color';
@@ -41,10 +41,37 @@ botao.innerText = 'Cores aleatórias';
 corpo.appendChild(botao);
 
 botao.addEventListener('click', function () {
+  let arrayDeCores = [];
   for (let index = 1; index < paletasGeral.length; index += 1) {
     let red = Math.floor(Math.random() * 256);
     let green = Math.floor(Math.random() * 256);
     let blue = Math.floor(Math.random() * 256);
     paletasGeral[index].style.backgroundColor = `rgb(${red},${green},${blue})`
+    arrayDeCores.push(paletasGeral[index].style.backgroundColor)
   }
+
+localStorage.setItem('colorPalette', JSON.stringify(arrayDeCores))
 })
+
+// Implementando o quinto requisito 2a parte
+
+window.onload = inicializando
+
+function inicializando() {
+  let arrayDeInformacoes = JSON.parse(localStorage.getItem('colorPalette')) || [];
+  if (arrayDeInformacoes !== []) {
+   for (let index = 0; index < arrayDeInformacoes.length; index += 1) {
+    paletasGeral[index + 1].style.backgroundColor = arrayDeInformacoes[index];
+}
+}
+else{
+    paletaBlack = document.getElementsByClassName('color')[0];
+    paletaBlack.style.backgroundColor = 'black';
+    paleta2 = document.getElementsByClassName('color')[1];
+    paleta2.style.backgroundColor = 'red';
+    paleta3 = document.getElementsByClassName('color')[2];
+    paleta3.style.backgroundColor = 'blue';
+    paleta4 = document.getElementsByClassName('color')[3];
+    paleta4.style.backgroundColor = 'green';
+}
+}
