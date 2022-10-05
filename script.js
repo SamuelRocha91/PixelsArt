e2bcb17d48c87fe8c1f7930a57acc7e098a0b70d
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-10-04 23:39:03 -0300

    questão 13

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-const h1 = document.createElement('h1');
+// Variáveis Globais
 const corpo = document.body;
-h1.id = 'title';
-h1.innerText = 'Paleta de Cores';
-corpo.appendChild(h1);
+const paletasGeral = document.getElementsByClassName('color');
 
-// Implementando segundo e terceiro requisitos
+// Primeira Questão
+function createH1() {
+  const h1 = document.createElement('h1');
+  h1.id = 'title';
+  h1.innerText = 'Paleta de Cores';
+  corpo.appendChild(h1);
+}
+createH1();
 
+// Segunda e Terceira Questões
 const paleta = document.createElement('div');
 paleta.id = 'color-palette';
 paleta.style.display = 'flex';
 corpo.appendChild(paleta);
+console.log(paleta)
+
 function criaMiniPaletas() {
   for (let index = 0; index < 4; index += 1) {
     const div = document.createElement('div');
@@ -21,25 +29,23 @@ function criaMiniPaletas() {
     paleta.appendChild(div);
   }
 }
-criaMiniPaletas()
+criaMiniPaletas();
 
-let paletaBlack = document.getElementsByClassName('color')[0];
+const paletaBlack = document.getElementsByClassName('color')[0];
 paletaBlack.style.backgroundColor = 'black';
-let paletasGeral = document.getElementsByClassName('color');
-let paleta2 = document.getElementsByClassName('color')[1];
+const paleta2 = document.getElementsByClassName('color')[1];
 paleta2.style.backgroundColor = 'red';
-let paleta3 = document.getElementsByClassName('color')[2];
+const paleta3 = document.getElementsByClassName('color')[2];
 paleta3.style.backgroundColor = 'blue';
-let paleta4 = document.getElementsByClassName('color')[3];
+const paleta4 = document.getElementsByClassName('color')[3];
 paleta4.style.backgroundColor = 'green';
 
-// Implementando o quarto requisito e o quinto 1a parte
+// Quarta questão
 
 const botao = document.createElement('button');
 botao.id = 'button-random-color';
 botao.innerText = 'Cores aleatórias';
 paleta.appendChild(botao);
-
 botao.addEventListener('click', function () {
   let arrayDeCores = [];
   for (let index = 1; index < paletasGeral.length; index += 1) {
@@ -49,55 +55,18 @@ botao.addEventListener('click', function () {
     paletasGeral[index].style.backgroundColor = `rgb(${red},${green},${blue})`
     arrayDeCores.push(paletasGeral[index].style.backgroundColor)
   }
-
+// QUINTA QUESTÃO - PRIMEIRA PARTE - SALVANDO NO LOCAL STORAGE AS CORES ALEATÓRIAS
 localStorage.setItem('colorPalette', JSON.stringify(arrayDeCores))
-
 })
 
-// Implementando o quinto requisito 2a parte
-
-window.onload = inicializando
-
-function inicializando() {
-  let arrayDeInformacoes = JSON.parse(localStorage.getItem('colorPalette')) || [];
-  if (arrayDeInformacoes !== []) {
-   for (let index = 0; index < arrayDeInformacoes.length; index += 1) {
-    paletasGeral[index + 1].style.backgroundColor = arrayDeInformacoes[index];
-}
-}
-else{
-    paletaBlack = document.getElementsByClassName('color')[0];
-    paletaBlack.style.backgroundColor = 'black';
-    paleta2 = document.getElementsByClassName('color')[1];
-    paleta2.style.backgroundColor = 'red';
-    paleta3 = document.getElementsByClassName('color')[2];
-    paleta3.style.backgroundColor = 'blue';
-    paleta4 = document.getElementsByClassName('color')[3];
-    paleta4.style.backgroundColor = 'green';
-}
-// continuação da solução da 12a questão
-let paletaSalva = JSON.parse(localStorage.getItem('pixelBoard')) || [];
-let miniPixels = quadro.children;
-
-// lá vai a loucura. Organizar!!! complexidade que pode não ser compreendida depois
-if (paletaSalva !== []){
-  for(let index = 0; index < paletaSalva.length; index += 1){
-        let chave = paletaSalva[index]
-        console.log(chave)
-      for(let index2 in chave){
-        console.log(index2)
-        miniPixels[index2].style.backgroundColor = chave[index2]
-      }
-      }
-    }
-}
-
-// Implementando solução do décimo primeiro requisito
-
+//DÉCIMA PRIMEIRA QUESTÃO INSERIDA EM CIMA PARA FORÇAR O BOTÃO A SER INSERIDO APÓS A PALETA
+const section = document.createElement('section')
+section.id = 'list-buttons'
+corpo.appendChild(section);
 const botao2 = document.createElement('button');
 botao2.id = 'clear-board';
 botao2.innerText = 'Limpar';
-corpo.appendChild(botao2);
+section.appendChild(botao2);
 
 botao2.addEventListener('click', function(){
   let miniPixels = quadro.children;
@@ -106,32 +75,34 @@ botao2.addEventListener('click', function(){
   }
 })
 
-// Implementando resolução do 6o e 7o requisito
-
+// SEXTA QUESTÃO + SÉTIMA QUESTÃO: QUADRADOS DE PIXEL
 let quadro = document.createElement('div');
+function criaQuadro (resolucao){
 quadro.id = 'pixel-board';
-quadro.style.width = '210px';
-quadro.style.height = '210px';
+quadro.style.width = `${resolucao * 42}px`;
+quadro.style.height = `${resolucao * 42} px`;
 quadro.style.margin = '50px';
 corpo.appendChild(quadro);
-
-
-function criaPixels() {
-  for(let index = 0; index < 25; index += 1){
-    let miniPixels = document.createElement('div');
-    miniPixels.className = 'pixel'
-    miniPixels.style.backgroundColor = 'white';
-    miniPixels.style.display = 'inline-block';
-    miniPixels.style.width = '40px';
-    miniPixels.style.height = '40px',
-    miniPixels.style.border = '1px solid black';
-    miniPixels.id = index
-    quadro.appendChild(miniPixels)
-  }
 }
-criaPixels()
+criaQuadro(5)
 
-// Implementando solução do oitavo e nono requisitos
+  
+function criaPixels(resolucao) {
+  for(let index = 0; index < resolucao * resolucao; index += 1){
+      let miniPixels = document.createElement('div');
+      miniPixels.className = 'pixel'
+      miniPixels.style.backgroundColor = 'white';
+      miniPixels.style.display = 'inline-block';
+      miniPixels.style.width = '40px';
+      miniPixels.style.height = '40px',
+      miniPixels.style.border = '1px solid black';
+      miniPixels.id = index
+      quadro.appendChild(miniPixels)
+    }
+  }
+  criaPixels(5)
+
+// OITAVA QUESTÃO E NONA QUESTÃO
 paletaBlack.className += ' selected';
 
 paleta.addEventListener('click', function (evento) {
@@ -141,11 +112,15 @@ paleta.addEventListener('click', function (evento) {
   paletaSelecionada.className += ' selected'
 })
 
-// Implementado solução do décimo requisito e do décimo primeiro requisito parte de salvar dados no local storage
+// DÉCIMA QUESTÃO
+
 let positionColor = [];
+
 quadro.addEventListener('click', function (evento) {
   let corSelecionada = document.querySelector('.selected').style.backgroundColor;
   let pequenoPixel = evento.target;
+  pequenoPixel.style.backgroundColor = corSelecionada;
+// DÉCIMA SEGUNDA QUESTÃO PRIMEIRA PARTE / salvando no localstorage
   let objetoDeCores = {
  
   }
@@ -155,3 +130,82 @@ quadro.addEventListener('click', function (evento) {
   localStorage.setItem('pixelBoard', JSON.stringify(positionColor))
 })
 
+// QUESTÃO TREZE
+
+function reset () {
+  quadro.remove()
+  const pixel = document.getElementsByClassName('pixel');
+  for (let index = 0; index < pixel.length; index += 1) {
+    pixel[index].remove();
+  }
+}
+
+const input = document.createElement('input');
+input.id = 'board-size';
+input.type = 'number';
+input.min = '1';
+const botaoInput = document.createElement('button');
+botaoInput.id = 'generate-board';
+botaoInput.innerText = 'VQV';
+section.appendChild(input);
+section.appendChild(botaoInput)
+
+botaoInput.addEventListener('click', function() {
+resolucao = input.value
+console.log(resolucao)
+if(resolucao === ""){
+  window.alert('Board Inválido!')
+  localStorage.clear();
+  reset();
+  quadro = document.createElement('div');
+  quadro.id = 'pixel-board';
+  resolucao = 5
+  quadro.style.width = `${resolucao * 42}px`;
+  quadro.style.height = `${resolucao * 42} px`;
+  quadro.style.margin = '50px';
+  corpo.appendChild(quadro);
+  criaPixels(5)
+}else{
+  reset()
+  quadro = document.createElement('div');
+  quadro.id = 'pixel-board';
+  quadro.style.width = `${resolucao * 42}px`;
+  quadro.style.height = `${resolucao * 42} px`;
+  quadro.style.margin = '50px';
+  corpo.appendChild(quadro);
+  criaPixels(resolucao);
+}
+})
+
+//QUINTA QUESTÃO SEGUNDA PARTE: RECUPERANDO AS INFORMAÇÕES SALVAS APÓS RECARREGAR A PÁGINA
+window.onload = function (){
+  let arrayDeInformacoes = JSON.parse(localStorage.getItem('colorPalette')) || [];
+  if (arrayDeInformacoes !== []) {
+   for (let index = 0; index < arrayDeInformacoes.length; index += 1) {
+    paletasGeral[index + 1].style.backgroundColor = arrayDeInformacoes[index];
+  }
+  }
+  else{
+    paletaBlack = document.getElementsByClassName('color')[0];
+    paletaBlack.style.backgroundColor = 'black';
+    paleta2 = document.getElementsByClassName('color')[1];
+    paleta2.style.backgroundColor = 'red';
+    paleta3 = document.getElementsByClassName('color')[2];
+    paleta3.style.backgroundColor = 'blue';
+    paleta4 = document.getElementsByClassName('color')[3];
+    paleta4.style.backgroundColor = 'green';
+  }
+  // DÉCIMA SEGUNDA QUESTÃO 2A PARTE: TRAZENDO DO LOCAL STORAGE
+  let paletaSalva = JSON.parse(localStorage.getItem('pixelBoard')) || [];
+  let miniPixels = quadro.children;
+  if (paletaSalva !== []){
+    for(let index = 0; index < paletaSalva.length; index += 1){
+        let chave = paletaSalva[index]
+        console.log(chave)
+      for(let index2 in chave){
+        console.log(index2)
+        miniPixels[index2].style.backgroundColor = chave[index2]
+      }
+      }
+    }
+ }
