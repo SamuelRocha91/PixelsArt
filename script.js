fcab4be1c452be514da1134ab3b217d055d706bc
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-03-16 20:30:07 -0300

    atualizações de css

--- script.js
+++ script.js
@@ -43,20 +43,21 @@ const paletasGeral = document.getElementsByClassName('color');
 
 const botao = document.createElement('button');
 botao.id = 'button-random-color';
+botao.className = 'btn btn-primary';
 botao.innerText = 'Cores aleatórias';
 paleta.appendChild(botao);
-botao.addEventListener('click', function mudaCor() {
-let arrayDeCores = [];
-for (let index = 1; index < paletasGeral.length; index += 1) {
-  let red = Math.floor(Math.random() * 256);
-  let green = Math.floor(Math.random() * 256);
-  let blue = Math.floor(Math.random() * 256);
-  paletasGeral[index].style.backgroundColor = `rgb(${red},${green},${blue})`
-  arrayDeCores.push(paletasGeral[index].style.backgroundColor)
+botao.addEventListener('click', () => {
+  const arrayDeCores = [];
+  for (let index = 1; index < paletasGeral.length; index += 1) {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    paletasGeral[index].style.backgroundColor = `rgb(${red},${green},${blue})`;
+    arrayDeCores.push(paletasGeral[index].style.backgroundColor);
   }
   // QUINTA QUESTÃO - PRIMEIRA PARTE - SALVANDO NO LOCAL STORAGE AS CORES ALEATÓRIAS
-  localStorage.setItem('colorPalette', JSON.stringify(arrayDeCores))
-})
+  localStorage.setItem('colorPalette', JSON.stringify(arrayDeCores));
+});
 
 // DÉCIMA PRIMEIRA QUESTÃO INSERIDA EM CIMA PARA FORÇAR O BOTÃO A SER INSERIDO APÓS A PALETA
 const section = document.createElement('section');
@@ -64,14 +65,15 @@ section.id = 'list-buttons';
 corpo.appendChild(section);
 const botao2 = document.createElement('button');
 botao2.id = 'clear-board';
+botao2.className = 'btn btn-warning';
 botao2.innerText = 'Limpar';
 section.appendChild(botao2);
 
-botao2.addEventListener('click', function () {
-  let miniPixels = quadro.children;
-  for( let index = 0; index < miniPixels.length; index += 1){
+botao2.addEventListener('click', () => {
+  const miniPixels = quadro.children;
+  for (let index = 0; index < miniPixels.length; index += 1) {
     miniPixels[index].style.backgroundColor = 'white';
-}
+  }
 });
 const salvadoraDeScript = document.createElement('div');
 salvadoraDeScript.id = 'maeDosDragoes';
@@ -91,7 +93,7 @@ criaQuadro(5);
 function criaPixels(resolucao) {
   for (let index = 0; index < resolucao * resolucao; index += 1) {
     const miniPixels = document.createElement('div');
-    miniPixels.className = 'pixel'
+    miniPixels.className = 'pixel';
     miniPixels.style.backgroundColor = 'white';
     miniPixels.style.display = 'inline-block';
     miniPixels.style.width = '40px';
@@ -106,16 +108,16 @@ criaPixels(5);
 // OITAVA QUESTÃO E NONA QUESTÃO
 paletaBlack.className += ' selected';
 
-paleta.addEventListener('click', function selecionaQuadrados(evento) {
-  let paletaSelecionada = evento.target;
-  let selected = document.querySelector('.selected');
+paleta.addEventListener('click', (evento) => {
+  const paletaSelecionada = evento.target;
+  const selected = document.querySelector('.selected');
   selected.classList.remove('selected');
-  paletaSelecionada.className += ' selected'
-  });
+  paletaSelecionada.className += ' selected';
+});
 
 // DÉCIMA QUESTÃO
 
-let positionColor = [];
+const positionColor = [];
 
 quadro.addEventListener('click', select);
 
@@ -124,7 +126,7 @@ function select(evento) {
   const pequenoPixel = evento.target;
   pequenoPixel.style.backgroundColor = corSelecionada;
   // DÉCIMA SEGUNDA QUESTÃO PRIMEIRA PARTE / salvando no localstorage
-  let objetoDeCores = {
+  const objetoDeCores = {
   };
   objetoDeCores[pequenoPixel.id] = corSelecionada; // forma encontrada para adicionar o número como chave. Jogar direto no objeto não funciona
   positionColor.push(objetoDeCores);
@@ -148,45 +150,44 @@ input.type = 'number';
 input.min = '1';
 const botaoInput = document.createElement('button');
 botaoInput.id = 'generate-board';
-botaoInput.innerText = 'VQV';
+botaoInput.className = 'btn btn-success';
+botaoInput.innerText = 'Aplicar';
 section.appendChild(input);
-section.appendChild(botaoInput)
+section.appendChild(botaoInput);
 
-botaoInput.addEventListener('click', function() {
-resolucao = input.value;
-  if(resolucao === ""){
-    window.alert('Board Inválido!')
+botaoInput.addEventListener('click', () => {
+  resolucao = input.value;
+  if (resolucao === '') {
+    window.alert('Board Inválido!');
     localStorage.clear();
     reset();
     quadro = document.createElement('div');
     criaQuadro(resolucao);
-    criaPixels(5)
-  } else {
-    if(resolucao < 5){
-      resolucao = 5;
-      reset()
-      quadro = document.createElement('div');
-      criaQuadro(resolucao);
-      criaPixels(resolucao);
+    criaPixels(5);
+  } else if (resolucao < 5) {
+    resolucao = 5;
+    reset();
+    quadro = document.createElement('div');
+    criaQuadro(resolucao);
+    criaPixels(resolucao);
   } else if (resolucao > 50) {
-      reset()
-      resolucao = 50;
-      quadro = document.createElement('div');
-      criaQuadro(resolucao);
-      criaPixels(resolucao);
+    reset();
+    resolucao = 50;
+    quadro = document.createElement('div');
+    criaQuadro(resolucao);
+    criaPixels(resolucao);
   } else {
-      reset()
-      quadro = document.createElement('div');
-      criaQuadro(resolucao);
-      criaPixels(resolucao);
-    }
+    reset();
+    quadro = document.createElement('div');
+    criaQuadro(resolucao);
+    criaPixels(resolucao);
   }
-  localStorage.setItem('boardSize', JSON.stringify(resolucao))
-  quadro.addEventListener('click', select) 
+  localStorage.setItem('boardSize', JSON.stringify(resolucao));
+  quadro.addEventListener('click', select);
 });
 // QUINTA QUESTÃO SEGUNDA PARTE: RECUPERANDO AS INFORMAÇÕES SALVAS APÓS RECARREGAR A PÁGINA
 
-quadro.addEventListener('click', select) 
+quadro.addEventListener('click', select);
 
 const arrayDeInformacoes = JSON.parse(localStorage.getItem('colorPalette')) || [];
 if (arrayDeInformacoes !== []) {
@@ -207,18 +208,18 @@ if (arrayDeInformacoes !== []) {
 // DÉCIMA SEGUNDA QUESTÃO 2A PARTE: TRAZENDO DO LOCAL STORAGE
 const paletaSalva = JSON.parse(localStorage.getItem('pixelBoard')) || [];
 const miniPixels = quadro.children;
-if (paletaSalva !== []){
+if (paletaSalva !== []) {
   for (let index = 0; index < paletaSalva.length; index += 1) {
-      const chave = paletaSalva[index]
-    for (let index2 in chave) {
+    const chave = paletaSalva[index];
+    for (const index2 in chave) {
       miniPixels[index2].style.backgroundColor = chave[index2];
     }
-    }
-    }
+  }
+}
 // DÉCIMA QUINTA É A RESPOSTA, MAS PRECISO REORGANIZAR MEU CÓDIGO
 window.onload = function () {
-  let param2 = JSON.parse(localStorage.getItem('boardSize')) || [];
-  let param3 = parseInt(param2)
+  const param2 = JSON.parse(localStorage.getItem('boardSize')) || [];
+  const param3 = parseInt(param2);
   if (isNaN(param3)) {
     console.log('cueca');
   } else {
